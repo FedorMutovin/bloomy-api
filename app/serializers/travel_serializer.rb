a53a3865abc9a9921275f5636d6f24d9ffffeaba
ArtemiyMutovin <artemmutovin@mail.ru>
2024-11-15 15:24:09 +0500

    add start_at and end_at columns to travel

--- app/serializers/travel_serializer.rb
+++ app/serializers/travel_serializer.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class TravelSerializer < Panko::Serializer
-  attributes :id, :description, :destination, :initiated_at
+  attributes :id, :description, :destination, :initiated_at, :start_at, :end_at
 
   def initiated_at
     object.initiated_at&.utc&.iso8601
+  end
+
+  def start_at
+    object.start_at&.utc&.iso8601
+  end
+
+  def end_at
+    object.end_at&.utc&.iso8601
   end
 end

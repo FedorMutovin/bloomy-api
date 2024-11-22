f935cf63314ad4183b1a10677b4530e9c1a2c3fb
ArtemiyMutovin <artemmutovin@mail.ru>
2024-11-22 15:23:32 +0500

    movie files fixes

--- app/contracts/movies/create_contract.rb
+++ app/contracts/movies/create_contract.rb
@@ -4,15 +4,15 @@ module Movies
   class CreateContract < ApplicationContract
     params do
       required(:name).filled(:string)
-      required(:status).filled(:string, included_in?: Movie::ALLOWED_STATUSES)
+      required(:status).filled(:string, included_in?: Statuses::Movie::ALLOWED_STATUSES)
       optional(:rating).filled(:string, included_in?: Movie::ALLOWED_RATINGS)
       optional(:completed_at).filled(:date_time)
       optional(:trigger).hash(Triggers::CreateContract.schema)
     end
 
     rule(:completed_at) do
-      if values[:status] == 'watched' && values[:completed_at].blank?
-        key.failure(I18n.t('errors.events.trackable.completed_at_required_when_watched'))
+      if values[:status] == Statuses::Movie::ALLOWED_STATUSES[0] && values[:completed_at].blank?
+        key.failure(I18n.t('errors.events.trackable.completed_at_required'))
       end
     end
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
-  ALLOWED_STATUSES = %w[watched waiting].freeze
   ALLOWED_RATINGS = %w[dislike not_worth_it neutral worth_to_watch interesting excited].freeze
   include Reflectable
   include Relatable
 
   belongs_to :user
   validates :name, :status, presence: true
-  validates :status, inclusion: { in: ALLOWED_STATUSES }
+  validates :status, inclusion: { in: Statuses::Movie::ALLOWED_STATUSES }
   validates :rating, inclusion: { in: ALLOWED_RATINGS, allow_nil: true }
 end
